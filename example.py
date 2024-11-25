7d796461cbf88261e1af0c952f7d82dfd654a077
Jimmy Ostler <jtostler1@gmail.com>
2024-11-25 15:06:51 -0800

    added plan

--- example.py
+++ example.py
@@ -28,4 +28,15 @@ for i in range(8):
 
 print()
 
-print(plane_partitions.is_plane_partition([[0, 1],[0,0]]))
+pprint([[0, 1],[0,0]])
+print("Is Partition:", plane_partitions.is_plane_partition([[0, 1],[0,0]]))
+
+print()
+
+part = [[13,13,13,13,13,13,13,11,11,9,9,7,7],[13,13,13,13,13,12,12,10,10,9,7,7,5],[13,13,13,13,13,11,10,10,8,8,6,5,5],[13,13,13,12,12,11,9,9,8,6,6,5,3],[13,13,13,12,10,10,8,7,6,6,4,4,3],[13,12,11,11,10,8,8,7,5,5,4,2,1],[13,12,10,9,8,8,6,6,4,3,2,2,1],[11,10,10,9,7,7,6,4,4,3,1,0,0],[11,10,8,8,6,5,4,4,2,2,1,0,0],[9,9,8,6,6,5,3,3,2,0,0,0,0],[9,7,6,6,4,4,2,1,1,0,0,0,0],[7,7,5,5,4,2,2,0,0,0,0,0,0],[7,5,5,3,3,1,1,0,0,0,0,0,0]]
+print("Complement = Rowmotion:", plane_partitions.complement(part) == plane_partitions.rowmotion(part))
+part = [[5,5,5,5,5],[5,5,5,4,4],[3,3,3,2,1],[3,1,0,0,0],[2,0,0,0,0]]
+for i in range(plane_partitions.rowmotion_orbit_length(part)):
+    part = plane_partitions.rowmotion(part)
+    print(plane_partitions.to_tikz_diagram(part))
+
